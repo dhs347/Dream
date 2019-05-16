b1cd2fdeaf74fca22b29aad12f5ea9e1a41a2892
jslijin <42512810+jslijin@users.noreply.github.com>
2019-05-16 16:53:14 +0800

    Update LinearRecursion.cpp

--- Template/Math/LinearRecursion.cpp
+++ Template/Math/LinearRecursion.cpp
@@ -1,5 +1,6 @@
 // a_{m} = \sum_{j=0}^{m−1}a_{j}*c_{j} O(m^2lgn)
 int linear_recurrence(ll n, int m, vi a, vi c) {
+	if (n<m) return (a[n]+P)%P;
 	vector<ll> v(m, 0), u(m<<1, 0);
 	v[0] = 1;
 	for(ll x = 0, W = n ? 1ll<<(63 - __builtin_clzll(n)) : 0; W; W >>= 1, x <<= 1) {
@@ -14,5 +15,5 @@ int linear_recurrence(ll n, int m, vi a, vi c) {
 	}
 	ll ans = 0;
 	rep(i, 0, m) (ans += v[i] * a[i]) %= P;
-	return ans;
+	return (ans+P)%P;
 }
