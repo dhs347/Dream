e9410d6b2228d51bc3c7c3e90fd6b3bb728e44c2
fblogy <39649744+fblogy@users.noreply.github.com>
2019-06-23 13:19:39 +0800

    Update Dinic.cpp

--- Template/Graph/Dinic.cpp
+++ Template/Graph/Dinic.cpp
@@ -1,35 +1,35 @@
- // [0,n) init!!
+// [0,n) init!!
 template<class T>
 struct Dinic{
     const static int N = 10101 , M = N * 10;
-    int s , t , n , h[N] , cur[N] , level[N] , q[N] , e , ne[M] , to[M];
+    int s , t , n , h[N] , cur[N] , lv[N] , q[N] , e , ne[M] , to[M];
     T cap[M] , flow;
     void liu(int u,int v,T w){ to[e] = v;ne[e] = h[u];cap[e] = w;h[u] = e++;}
     void link(int u,int v,T w){ liu(u , v , w);liu(v , u , 0);}
     void ini(int _n = N) { fill(h , h + (n=_n) , -1);e = 0;}
     bool bfs(){
         int L = 0 , R = 0;
-        fill(level , level + n , -1);
-        level[q[R++] = s] = 0;
-        while(L < R && level[t] == -1){
+        fill(lv , lv + n , -1);
+        lv[q[R++] = s] = 0;
+        while(L < R && !~lv[t]){
             int c = q[L++];
-            for(int k=h[c];~k;k=ne[k])
-                if(cap[k] > 0 && level[to[k]] == -1)
-                    level[q[R++] = to[k]] = level[c] + 1;
+            for(int k = h[c]; ~k ; k = ne[k])
+                if(cap[k] > 0 && !~lv[to[k]])
+                    lv[q[R++] = to[k]] = lv[c] + 1;
         }
-        return ~level[t];
+        return ~lv[t];
     }
     T dfs(int c,T mx){
         if(c == t) return mx;
         T ret = 0;
-        for(int &k = cur[c];~k;k = ne[k]){
-            if(level[to[k]] == level[c] + 1 && cap[k] > 0){
+        for(int &k = cur[c]; ~k; k = ne[k]){
+            if(lv[to[k]] == lv[c] + 1 && cap[k] > 0){
                 T flow = dfs(to[k] , min(mx , cap[k]));
                 ret += flow;cap[k] -= flow , cap[k^1] += flow;mx -= flow;
                 if(!mx) return ret;
             }
         }
-        level[c] = -1;
+        lv[c] = -1;
         return ret;
     }
     T run(int _s,int _t){
@@ -37,7 +37,7 @@ struct Dinic{
         flow = 0;
         while(bfs()){
             copy(h , h + n , cur);
-            flow += dfs(s,~0U>>1);
+            flow += dfs(s, ~0U>>1);
         }
         return flow;
     }
