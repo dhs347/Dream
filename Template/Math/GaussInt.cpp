cc845bac50782ae6e21ba7d50b199ba4a423aaa9
fblogy <39649744+fblogy@users.noreply.github.com>
2019-06-26 11:23:02 +0800

    高斯消元

--- Template/Math/GaussInt.cpp
+++ Template/Math/GaussInt.cpp
@@ -1,45 +1,41 @@
 namespace GaussInt{
 	static const int N = ::N, P = 1e9 + 7;
 	int a[N][N], x[N]; //增广矩阵和解集
-	bool ok[N]; // 标记变元是否确定
-	int free[N], free_num; // 一组合法自由变元 
+	int free[N], fnum, k, col, p; // 一组合法自由变元 
 	int add(int a, int b) {if ((a += b) >= P) a -= P; return a < 0 ? a + P : a;}
 	int mul(int a, int b) {return 1ll * a * b % P;}
 	int kpow(int a, int b) {int r=1;for(;b;b>>=1,a=mul(a,a)) {if(b&1)r=mul(r,a);}return r;}
+	
+	void genx() {
+		int pre = var; fnum = 0;
+		per(i, 0, k) {
+			rep(j, 0, var) if (a[i][j]) { p = j; break; }
+			rep(j, 0, i) if (a[j][p]) {
+				int t = a[j][p];
+				rep(l, p, var+1) a[j][l] = add(a[j][l], -mul(a[i][l], t));
+			}
+			rep(j, p+1, pre) free[fnum++] = j, x[j] = (?); pre = p; 
+			x[p] = a[i][var];
+			rep(j, p+1, var) x[p] = add(x[p], -mul(a[i][j], x[j]));
+		}
+		rep(j, 0, pre) free[fnum++] = j;
+	}
+	
 	int Gauss(int equ, int var){
-		int k, col, p;
-		fill_n(free, var, 0); free_num = 0;
-		fill_n(x, var, 0);
 		for(k = col = 0; k < equ && col < var; ++k, ++col){
-			p = k; rep(i, k+1, equ) if (a[i][col]) {p = i; break;}
-			if (p != k) rep(j, k, var+1) swap(a[p][j], a[k][j]);
+			p = k; rep(i, k, equ) if (a[i][col]) {p = i; break;}
+			if (p != k) rep(j, col, var+1) swap(a[p][j], a[k][j]);
 			if(!a[k][col]) {k--; continue;}
 			int inv = kpow(a[k][col], P - 2);
 			rep(i, col, var+1) a[k][i] = mul(a[k][i], inv);
-			rep(i, k+1, equ){
-				if(!a[i][col]) continue;
+			rep(i, k+1, equ) if (a[i][col]) {
 				int t = a[i][col];
 				rep(j, col, var+1) a[i][j] = add(a[i][j], -mul(a[k][j], t));
 			}
 		}
 		rep(i, k, equ) if (a[i][var]) return -1;//无解
 		if(k < var){
-			/*
-			int pre = var;
-			per(i, 0, k) {
-				int num = 0;
-				rep(j, 0, var) if (a[i][j]) {
-					if (!num) p = j; num++;
-				}
-				rep(j, 0, i) if (a[j][p]) {
-					int t = a[j][p];
-					rep(l, p, var+1) a[j][l] = add(a[j][l], -mul(a[i][l], t));
-				}
-				rep(j, p+1, pre) free[free_num++] = j; pre = p; 
-				if(num > 1) continue;
-				x[p] = a[i][var];
-				ok[p] = 1;
-			}*/
+			//genx();
 			return var - k;//自由变元个数
 		}
 		per(i, 0, var) {
--- Template/Math/GaussXor.cpp
+++ Template/Math/GaussXor.cpp
@@ -3,42 +3,42 @@ namespace Gauss{
 	static const int N = 2e3 + 10;
 	//有 equ 个方程， var 个变元。增广矩阵行数为 equ ,列数为 [0..var]
 	bitset<N> a[N]; //增广矩阵 modif
-	bool ok[N]; // 标记变元是否确定 
 	int x[N]; //解集
-	int free[N], free_num; //一组合法自由变元（多解枚举自由变元可以使用）
+	int p, col, k; // k 为增广矩阵的秩
+	int free[N], fnum; //一组合法自由变元（多解枚举自由变元可以使用）
 	//返回值为 -1 表示无解，为 0 是唯一解，否则返回自由变元个数
+	void genx(int msk) {
+		rep(i, 0, fnum) x[free[i]] = (msk >> i) & 1;
+		per(i, 0, k) {
+			rep(j, 0, var) if(a[i][j]) { p = j; break; }	
+			x[p] = a[i][var];
+			rep(j, p+1, var) x[p] ^= (a[i][j] && x[j]);
+		}
+	}
+	
 	int Gauss(int equ, int var){
-		int p, col, k; // k 为增广矩阵的秩
-		fill_n(ok, var, 0); free_num = 0;
-		fill_n(x, var, 0);
+		fnum = 0;
 		for(k = 0, col = 0; k < equ && col < var; k++, col++){
 			p = k; rep(i, k, equ) if (a[i][col]) {p = i; break;}
 			if (p != k) swap(a[k], a[p]);
 			if (!a[k][col]){
-				k--; free[free_num++] = col;//这个是自由变元
+				k--; free[fnum++] = col;//这个是自由变元
 				continue;
 			}
 			rep(i, 0, equ) if (i != k && a[i][col]) a[i] ^= a[k];
 		}
-		rep(i, col, var) free[free_num++] = i;
+		rep(i, col, var) free[fnum++] = i;
 		rep(i, k, equ) if (a[i][var]) return -1;
 		if(k < var) {
+			// genx();
 			/*
-			per(i, 0, k) {
-				int num = 0;
-				rep(j, 0, var) if(a[i][j]) {
-					if (!num) p = j; num++;
-				}
-				if(num > 1) continue;
-				ok[p] = 1; x[p] = a[i][var];
-			}*/
+			
 			return var - k;//自由变元个数
 		} 
 		//唯一解，回代
 		per(i, 0, var){
-			bool t = a[i][var];
-			rep(j, i+1, var) t ^= (a[i][j] && x[j]);
-			x[i] = t;
+			x[i] = a[i][var];
+			rep(j, i+1, var) x[i] ^= (a[i][j] && x[j]);
 		}
 		return 0;
 	}
