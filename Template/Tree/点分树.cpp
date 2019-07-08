e7320879b3f2a1db0f1b1218dfa293e5b30b9782
Wowkiee <2240134402@qq.com>
2019-07-08 13:23:54 +0800

    点分树

--- Template/Tree/点分树.cpp
+++ Template/Tree/点分树.cpp
@@ -1,19 +1,24 @@
 // id starts from 1
 namespace Centriod {
 	const int N = ::N;
-	bool vis[N]; int sz[N];
+	bool vis[N]; int sz[N], par[N]; vi G;
 	void dfssz(int c,int fa,int Sz,int &rt){
 		sz[c] = 1;
 		for(auto t : g[c]) if(!vis[t]&&t!=fa) dfssz(t,c,Sz,rt) , sz[c]+=sz[t];
 		if(!rt && sz[c]*2>Sz) rt=c;
 	}
-	void dfs(int c){
+	int dfs(int c){
 		int rt=0;dfssz(c,0,0,rt);dfssz(c,0,sz[c],rt=0);
 		vis[rt] = true;
-		/*
-		 * 注意计算以 rt 为起点的路径、只包含 rt 的路径
-		 * 注意 v != vis[rt] 
-		 */
-		for(auto t : g[rt]) if(!vis[t]) dfs(t);
+		for(auto v : g[rt]) if(!vis[v]) {
+			int t = dfs(v);
+			G[rt].pb(t);
+			par[t] = rt;
+		}
+		return rt;
+	}
+	void init() { 
+		fill_n(G + 1, n, vi()); 
+		fill_n(par + 1, n, 0);
 	}
 };
