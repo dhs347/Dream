010d43d30ca9e3a4765a9196b05acec9a5a72956
jslijin <42512810+jslijin@users.noreply.github.com>
2019-07-17 16:12:29 +0800

    123

---
+++ Code_Lib/hdu4861_lj.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define rep(i, a, b) for(int i=(a); i<(b); i++)
+#define per(i, a, b) for(int i=(b)-1; i>=(a); i--)
+#define sz(a) (int)a.size()
+#define de(a) cout << #a << " = " << a << endl
+#define dd(a) cout << #a << " = " << a << " "
+#define all(a) a.begin(), a.end()
+#define pw(x) (1ll<<(x))
+#define endl "\n"
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef double db;
+
+void file_put() {
+    freopen("filename.in", "r", stdin);
+    freopen("filename.out", "w", stdout);
+}
+
+ll k,p;
+
+int main() {
+//    file_put();
+    
+    while (scanf("%lld%lld",&k,&p)!=EOF) {
+        if ((k/(p-1))&1) printf("YES\n"); else printf("NO\n");
+    }
+    
+	return 0;
+}
+
