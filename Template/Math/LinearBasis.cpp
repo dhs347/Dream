293373c48b7a140c8b40f316e8b3beacf8612cba
jslijin <42512810+jslijin@users.noreply.github.com>
2019-08-02 10:52:19 +0800

    Update LinearBasis.cpp

--- Template/Math/LinearBasis.cpp
+++ Template/Math/LinearBasis.cpp
@@ -32,3 +32,33 @@ struct LB{
     }
 } B[N];
 
+// 集合线性基求交与查询
+const int M=33;
+ll tmp[M];
+struct LB{
+    ll a[M]; 
+    LB() { mem(a,0); }
+    void Clear() { mem(a,0); }
+    void Copy(LB &A) { rep(i,0,M) a[i]=A.a[i]; }
+	// 向 this 中插入 x ， 返回 y 在后来插入元素中的投影
+    bool I(ll x,ll &y) {
+        y=x;
+        for(int i=M-1; ~i && x; --i) if (x>>i&1)
+            if (a[i]) x^=a[i],y^=tmp[i]; else { a[i]=x,tmp[i]=y; return 1; }
+        return 0;
+    }
+    bool Q(ll x) {
+        for(int i=M-1; ~i && x; --i) if (x>>i&1)
+            if (a[i]) x^=a[i]; else return 0;
+        return 1;
+    }
+    friend void Intersect(LB &A,LB &B,LB &C) {
+        LB AA; ll y,z; AA.Copy(A),C.Clear(); mem(tmp,0);
+        per(i,0,M) if (B.a[i]) if (!AA.I(B.a[i],y)) C.I(y,z);
+    }
+	// 化为最简型 ，方便线性空间的 hash
+    void build () {
+        per(i,0,M) per(j,0,i) a[i]=min(a[i],a[i]^a[j]);
+    }
+    void print() { debug_arr(a,M-1); }
+};
