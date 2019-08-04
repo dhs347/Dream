fb7d15a13992e0acb8e6474e0767664cd9fde4a9
fblogy <39649744+fblogy@users.noreply.github.com>
2019-08-04 10:32:39 +0800

    Update e.cpp

--- Team_Training/17、2019-nowcoder-multi-6/code/e.cpp
+++ Team_Training/17、2019-nowcoder-multi-6/code/e.cpp
@@ -19,54 +19,43 @@ typedef vector<int> vi;
  
 const int N = 2020;
  
-int ans[N][N], use[N][N], tmp[N][N], p[N];
+int ans[N][N], use[N][N], ans2[N][N], p[N], m;
 void get(int n) {
-    if (n == 4) {
-        rep(i, 1, 4+1) rep(j, 1, 5) ans[i][j] = 0;
-        ans[1][2] = ans[2][1] = 1;
-        ans[2][3] = ans[3][2] = 1;
-        ans[3][4] = ans[4][3] = 1;
-        p[1] = 2; p[2] = 4; p[3] = 1; p[4] = 3;
-        return;
-    }
-    get(n / 2);
-    int m = n / 2;
-    rep(i, m+1, n+1) p[i]  = p[i - m] + m;
-    rep(i, 1, n+1) rep(j, 1, n+1) use[i][j] = 0;
-    rep(i, 1, n+1) rep(j, i+1, n+1) {
-        if (i <= m && j <= m) tmp[i][j] = ans[i][j];
-        if (i > m && j > m) tmp[i][j] = ans[i - m][j - m];
-        if (i <= m && j > m) {
-            if (use[i][j] == 0 && use[p[i]][p[j]] != 1) {
-                use[i][j] = 1;
-                use[p[i]][p[j]] = 2;
-                tmp[i][j] = 1;
-            }
-            if (use[p[i]][p[j]] == 1) use[i][j] = 2;
-        }
-    }
-    rep(i, 1, n+1) rep(j, i+1, n+1) tmp[j][i] = tmp[i][j];
-    rep(i, 1, n+1) rep(j, 1, n+1) ans[i][j] = tmp[i][j];
+	int m = n / 4;
+	rep(i, 1, m+1) rep(j, i+1, m+1) ans[i][j] = ans[j][i] = 1;
+	rep(i, n - m + 1, n+1) rep(j, i + 1, n+1) ans[i][j] = ans[j][i] = 1;
+	rep(i, 1, m+1) rep(j, m+1, m+m+1) ans[i][j] = ans[j][i] = 1;
+	rep(i, m+1, m+m+1) rep(j, m+m+1, m+m+m+1) ans[i][j] = ans[j][i] = 1;
+	rep(i, m+m+1, m+m+m+1) rep(j, m+m+m+1, m+m+m+m+1) ans[i][j] = ans[j][i] = 1;
+	rep(i, 1, m+1) p[i] = i + m;
+	rep(i, m+1, m+m+1) p[i] = i + m + m;
+	rep(i, m+m+1, m+m+m+1) p[i] = i - m - m;
+	rep(i, m+m+m+1, m+m+m+m+1) p[i] = i - m;
+
 }
  
 void solve() {
     int n; cin >> n;
-    if(n % 4 > 1) { cout << "No" << endl; return ; }
+    if((n % 4) > 1) { cout << "No" << endl; return ; }
     cout << "Yes" << endl;
     if(n == 1) {
         cout << 0 << endl << 1 << endl;
         return ;
     }
-    if(n == 4) {
-        cout << "0100" << endl;
-        cout << "1010" << endl;
-        cout << "0101" << endl;
-        cout << "0010" << endl;
-        cout << "2 4 1 3" << endl;
-        return ;
+    rep(i, 1, n+1) rep(j, 1, n+1) ans[i][j] = 0;
+    if (n & 1) {
+		get(n - 1);
+		m = (n - 1) / 4;
+		rep(i, 1, m+1) ans[i][n] = ans[n][i] = 1;
+		rep(i, n - m, n) ans[i][n] = ans[n][i] = 1;
+		p[n] = n;
+	}else get(n);
+	rep(i, 1, n+1) rep(j, 1, n+1) if (i != j) ans2[i][j] = ans[i][j] ^ 1;
+	rep(i, 1, n+1) rep(j, 1, n+1) if (i != j) assert(ans[i][j] == ans2[p[i]][p[j]]);
+	rep(i, 1, n+1) {
+        rep(j, 1, n+1) cout << ans[i][j];
+        cout << endl;
     }
-    if (n == 2 || n == 3) { cout << "No"; return; }
-    if ()
     rep(i, 1, n+1) cout << p[i] << " \n"[i == n];
 }
  
