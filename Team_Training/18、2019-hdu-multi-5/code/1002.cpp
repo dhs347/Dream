b9ef6d7ea097ce10c660f0be55e6633de78436ca
wuyuanyuan <2240134402@qq.com>
2019-09-09 14:52:27 +0800

    18、2019-hdu-multi-5 B

--- Team_Training/18、2019-hdu-multi-5/code/1002.cpp
+++ Team_Training/18、2019-hdu-multi-5/code/1002.cpp
@@ -2,89 +2,78 @@
 using namespace std;
 #define fi first
 #define se second
-#define pb push_back
 #define mp make_pair
-#define rep(i, a, b) for(int i = (a); i < (b); ++i)
-#define per(i, a, b) for(int i = (b) - 1; i >= (a); --i)
-#define de(x) cout << #x << " = " << x << endl
-#define dd(x) cout << #x << " = " << x << " "
+#define pb push_back
+#define rep(i, a, b) for(int i=(a); i<(b); i++)
+#define per(i, a, b) for(int i=(b)-1; i>=(a); i--)
+#define sz(a) (int)a.size()
+#define de(a) cout << #a << " = " << a << endl
+#define dd(a) cout << #a << " = " << a << " "
+#define all(a) a.begin(), a.end()
 #define pw(x) (1ll<<(x))
-#define all(x) x.begin(), x.end()
-#define sz(x) (int)x.size()
 #define endl "\n"
 typedef long long ll;
 typedef pair<int, int> pii;
-typedef double db;
 typedef vector<int> vi;
 
 const int N = 101010, M = 30;
 
-int n, a[N], b[N], ne[N * M][2], L, cnt[N * M], rt, num[N * M];
-vi vec[N * M];
-bool vis[N];
-priority_queue<pair<int, pii> > que;
+int n, a[2][N], ne[2][N * M][2], cnt[2][N * M], L[2];
+vi ans;
 
-int newnode() {
-	fill_n(ne[L], 2, 0);
-	cnt[L] = num[L] = 0;
-	vec[L].clear();
-	return L++;
+void init(int ne[][2], int cnt[], int &L) {
+	rep(i, 0, L + 1) fill_n(ne[i], 2, 0), cnt[i] = 0; L = 0;
 }
-void add(int x) {
-	int p = rt;
-	per(i, 0, 30) {
-		int c = (x >> i & 1);
-		if(!ne[p][c]) ne[p][c] = newnode();
-		p = ne[p][c];
-		++cnt[p];
-	}
-	num[p] = x;
-}
-void go(int x, int i) {
-	int p = rt;
-	per(i, 0, 30) {
-		int c = (x >> i & 1);
-		if(ne[p][c] && cnt[ne[p][c]]) p = ne[p][c];
-		else p = ne[p][c ^ 1];
-	}
-	vec[p].pb(i);
-	que.push(mp(-(x ^ num[p]), mp(p, i)));
-}
-void del(int x) {
-	int p = rt;
-	per(i, 0, 30) {
-		int c = (x >> i & 1);
-		p = ne[p][c];
-		--cnt[p];
-	}
-	if(cnt[p]) return ;
-	for(auto u : vec[p]) if(!vis[u]) go(b[u], u);
-	vec[p].clear();
-}
-void solve() {
-	cin >> n;
-	rep(i, 1, n + 1) cin >> a[i];
-	rep(i, 1, n + 1) cin >> b[i], vis[i] = 0;
-	while(!que.empty()) que.pop();
-	L = 0; rt = newnode();
-	rep(i, 1, n + 1) add(a[i]);
-	rep(i, 1, n + 1) go(b[i], i);
+
+void gao(int a[], int ne[][2], int cnt[], int &L) {
+	init(ne, cnt, L);
 	rep(i, 1, n + 1) {
-		while(1) {
-			auto u = que.top(); que.pop();
-			if(!cnt[u.se.fi]) continue;
-			vis[u.se.se] = 1;
-			cout << -u.fi << " \n"[i == n];
-			del(num[u.se.fi]);
+	//	de(i);
+		cin >> a[i];
+		int p = 0; per(j, 0, M) {
+			int c = (a[i] >> j & 1);
+			if(!ne[p][c]) ne[p][c] = ++L;
+			p = ne[p][c]; ++cnt[p]; //de(p);
+		} 
+	}
+	//rep(i, 0, L + 1) dd(i), de(cnt[i]);
+}
+
+int work() {
+	int ans = 0, p = 0, q = 0;
+	rep(i, 0, M) {
+		int t = -1;
+		rep(o, 0, 2) if(cnt[0][ne[0][p][o]] && cnt[1][ne[1][q][o]]) {
+			--cnt[0][ne[0][p][o]], --cnt[1][ne[1][q][o]];
+			t = 0; p = ne[0][p][o], q = ne[1][q][o];
+		//	dd(i), dd(o), de(o);
 			break;
 		}
+		if(t == -1) rep(o, 0, 2) if(cnt[0][ne[0][p][o]] && cnt[1][ne[1][q][o ^ 1]]) {
+			--cnt[0][ne[0][p][o]], --cnt[1][ne[1][q][o ^ 1]];
+			t = 1; p = ne[0][p][o], q = ne[1][q][o ^ 1];
+		//	dd(i), dd(o), de((o ^ 1));
+			break;
+		}
+		ans = ans << 1 | t;
 	}
+	return ans; 
+}
+
+void solve() {
+	cin >> n;
+	rep(o, 0, 2) gao(a[o], ne[o], cnt[o], L[o]);
+	ans.clear();
+	rep(i, 1, n + 1) ans.pb(work());
+	sort(all(ans));
+	rep(i, 0, n) cout << ans[i] << " \n"[i == n - 1];
 }
 
 int main() {
-	std::ios::sync_with_stdio(0);
+	std::ios::sync_with_stdio(false);
 	std::cin.tie(0);
 	int T; cin >> T;
-	while(T--) solve();
+	while(T--) solve(); 
 	return 0;
 }
+
