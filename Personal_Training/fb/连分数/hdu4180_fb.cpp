cc1255a1679d684053a497fe824379d7d5fdab65
fblogy <39649744+fblogy@users.noreply.github.com>
2019-09-24 21:20:22 +0800

    Create hdu4180_fb.cpp

---
+++ Personal_Training/fb/连分数/hdu4180_fb.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define rep(i, a, b) for(int i=(a); i<(b); i++)
+#define per(i, a, b) for(int i=(b)-1; i>=(a); i--)
+#define sz(a) (int)a.size()
+#define de(a) cout << #a << " = " << a << endl
+#define dd(a) cout << #a << " = " << a << " "
+#define all(a) (a).begin(), (a).end()
+#define pw(x) (1ll<<(x))
+#define lb(x) ((x) & -(x))
+#define endl "\n"
+#define FI(x) freopen(#x".in","r",stdin)
+#define FO(x) freopen(#x".out","w",stdout)
+typedef double db;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+//typedef uniform_int_distribution<ll> RR;
+//mt19937 gen(998244353);
+//ll rnd(ll l, ll r) { RR dis(l, r); return dis(gen); }
+const int P = 1e9 + 7;
+int add(int a, int b) {if((a += b) >= P) a -= P; return a < 0 ? a + P : a;}
+int mul(int a, int b) {return 1ll * a * b % P;}
+int kpow(int a, int b) {int r=1;for(;b;b>>=1,a=mul(a,a)) {if(b&1)r=mul(r,a);}return r;}
+//----
+
+int x, y, T, a, b;
+
+void solve(int a, int b) {
+	if (b == 1) {	
+		x = 1; y = a / b - 1;
+		return;
+	}
+	solve(b, a % b);
+	x += y * (a / b);
+	swap(x, y);
+}
+
+int main() {
+	//FI(a);
+	//ios::sync_with_stdio(0);
+	//cin.tie(0);
+	//cout << setiosflags(ios::fixed);
+	//cout << setprecision(2);
+	cin >> T;
+	rep(cas, 0, T) {
+		scanf("%d/%d", &a, &b);
+		int d = __gcd(a, b);
+		a /= d; b /= d;
+		if (d > 1) {
+			cout << a << "/" << b << endl;
+			continue;
+		}
+		solve(b, a);
+		cout << x << "/" << y << endl;
+	}
+	return 0;
+}
+
+
